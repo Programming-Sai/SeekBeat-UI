e3022bdfc04beecaf8590b2bdb32ba0c3782823d
Programming-Sai <saiahprog6@gmail.com>
2025-09-11 05:27:02 +0000

    Working on the trimming logic for edits

--- components/HomeSideBar.jsx
+++ components/HomeSideBar.jsx
@@ -8,7 +8,8 @@ import {
   ScrollView,
   TouchableOpacity,
   Image,
-  Alert,
+  ImageBackground,
+  ActivityIndicator,
 } from "react-native";
 import * as Clipboard from "expo-clipboard";
 import { useTheme } from "../contexts/ThemeContext";
@@ -26,12 +27,17 @@ import { usePlayer } from "../contexts/PlayerContext";
 
 export const HomeSideBar = () => {
   const { theme, accentColors, accentKey } = useTheme();
-  const { data, removeSearchAt, removeDownload, getLastSearch } =
-    useAppStorage();
+  const {
+    data,
+    removeSearchAt,
+    removeDownload,
+    getLastSearch,
+    getDownloadStatus,
+  } = useAppStorage();
   const { submitSearch } = useSearch();
   const { getQueueIndex } = useDownloader();
   const [tab, setTab] = useState("history");
-  const { setQueueFromSearchResults, showMiniForIndex, queue } = usePlayer();
+  const { setQueueFromSearchResults, showMiniForIndex } = usePlayer();
 
   // local state to reflect quick UI changes (delete). Replace with persistence call as needed.
   const [localHistory, setLocalHistory] = useState(data?.searchHistory ?? []);
@@ -43,122 +49,9 @@ export const HomeSideBar = () => {
 
   // const downloads = [];
   const downloads = data?.downloads || [];
-  // console.log("DOWNLOADS: ", downloads);
-  // const downloads = [
-  //   {
-  //     title:
-  //       "LEADERS OF HISTORY RAP CYPHER | RUSTAGE ft. The Stupendium, Keyblade, TOPHAMHAT-KYO &amp; More",
-  //     duration: 643,
-  //     uploader: "RUSTAGE",
-  //     thumbnail: "https://i.ytimg.com/vi/PEwy4U1OkBA/hqdefault.jpg",
-  //     webpage_url: "https://www.youtube.com/watch?v=PEwy4U1OkBA",
-  //     upload_date: "2025-06-21T23:45:01Z",
-  //     largest_thumbnail: "https://i.ytimg.com/vi/PEwy4U1OkBA/hqdefault.jpg",
-  //     smallest_thumbnail: "https://i.ytimg.com/vi/PEwy4U1OkBA/default.jpg",
-  //   },
-  //   {
-  //     title:
-  //       "ALEXANDER THE GREAT RAP | &quot;TOO GREAT&quot; | RUSTAGE ft. McGwire",
-  //     duration: 165,
-  //     uploader: "RUSTAGE",
-  //     thumbnail: "https://i.ytimg.com/vi/ylkIN11u8MU/hqdefault.jpg",
-  //     webpage_url: "https://www.youtube.com/watch?v=ylkIN11u8MU",
-  //     upload_date: "2024-12-07T17:00:53Z",
-  //     largest_thumbnail: "https://i.ytimg.com/vi/ylkIN11u8MU/hqdefault.jpg",
-  //     smallest_thumbnail: "https://i.ytimg.com/vi/ylkIN11u8MU/default.jpg",
-  //   },
-  //   {
-  //     title: "Leaders of History Rap Cypher | Rustage | History Teacher Reacts",
-  //     duration: 1741,
-  //     uploader: "Mr. Terry History",
-  //     thumbnail: "https://i.ytimg.com/vi/DhLR9GS-muo/hqdefault.jpg",
-  //     webpage_url: "https://www.youtube.com/watch?v=DhLR9GS-muo",
-  //     upload_date: "2025-06-27T17:00:21Z",
-  //     largest_thumbnail: "https://i.ytimg.com/vi/DhLR9GS-muo/hqdefault.jpg",
-  //     smallest_thumbnail: "https://i.ytimg.com/vi/DhLR9GS-muo/default.jpg",
-  //   },
-  //   {
-  //     title:
-  //       "CID KAGENOU RAP | &quot;ATOMIC&quot; | RUSTAGE ft. TSUYO [THE EMINENCE IN SHADOW]",
-  //     duration: 172,
-  //     uploader: "RUSTAGE",
-  //     thumbnail: "https://i.ytimg.com/vi/i6urQLIEWBE/hqdefault.jpg",
-  //     webpage_url: "https://www.youtube.com/watch?v=i6urQLIEWBE",
-  //     upload_date: "2025-07-04T21:01:00Z",
-  //     largest_thumbnail: "https://i.ytimg.com/vi/i6urQLIEWBE/hqdefault.jpg",
-  //     smallest_thumbnail: "https://i.ytimg.com/vi/i6urQLIEWBE/default.jpg",
-  //   },
-  //   {
-  //     title:
-  //       "THOR, LOKI &amp; ODIN RAP | &quot;VALHALLA&quot; | RUSTAGE ft. Shwabadi &amp; Connor Quest!",
-  //     duration: 234,
-  //     uploader: "RUSTAGE",
-  //     thumbnail: "https://i.ytimg.com/vi/M6BRcXzcP_k/hqdefault.jpg",
-  //     webpage_url: "https://www.youtube.com/watch?v=M6BRcXzcP_k",
-  //     upload_date: "2024-10-11T22:00:12Z",
-  //     largest_thumbnail: "https://i.ytimg.com/vi/M6BRcXzcP_k/hqdefault.jpg",
-  //     smallest_thumbnail: "https://i.ytimg.com/vi/M6BRcXzcP_k/default.jpg",
-  //   },
-  //   {
-  //     title: "LEADERS OF HISTORY RAP CYPHER - Rustage Reaction",
-  //     duration: 1565,
-  //     uploader: "Vlogging Through History",
-  //     thumbnail: "https://i.ytimg.com/vi/5NVF1eg1x6Y/hqdefault.jpg",
-  //     webpage_url: "https://www.youtube.com/watch?v=5NVF1eg1x6Y",
-  //     upload_date: "2025-07-24T16:23:47Z",
-  //     largest_thumbnail: "https://i.ytimg.com/vi/5NVF1eg1x6Y/hqdefault.jpg",
-  //     smallest_thumbnail: "https://i.ytimg.com/vi/5NVF1eg1x6Y/default.jpg",
-  //   },
-  //   {
-  //     title:
-  //       "YONKO RAP CYPHER | RUSTAGE ft. Shwabadi, Connor Quest! PE$O PETE &amp; Lex Bratcher [ONE PIECE]",
-  //     duration: 248,
-  //     uploader: "RUSTAGE",
-  //     thumbnail: "https://i.ytimg.com/vi/8tCMJOYvpi4/hqdefault.jpg",
-  //     webpage_url: "https://www.youtube.com/watch?v=8tCMJOYvpi4",
-  //     upload_date: "2021-06-11T21:00:05Z",
-  //     largest_thumbnail: "https://i.ytimg.com/vi/8tCMJOYvpi4/hqdefault.jpg",
-  //     smallest_thumbnail: "https://i.ytimg.com/vi/8tCMJOYvpi4/default.jpg",
-  //   },
-  //   {
-  //     title:
-  //       "SUPERMAN RAP  | &quot;HOPE&quot; | RUSTAGE ft. JT MUSIC &amp; LongestSoloEver",
-  //     duration: 215,
-  //     uploader: "RUSTAGE",
-  //     thumbnail: "https://i.ytimg.com/vi/ayA_GJV93gA/hqdefault.jpg",
-  //     webpage_url: "https://www.youtube.com/watch?v=ayA_GJV93gA",
-  //     upload_date: "2025-08-09T00:35:00Z",
-  //     largest_thumbnail: "https://i.ytimg.com/vi/ayA_GJV93gA/hqdefault.jpg",
-  //     smallest_thumbnail: "https://i.ytimg.com/vi/ayA_GJV93gA/default.jpg",
-  //   },
-  //   {
-  //     title:
-  //       "WILL OF D. RAP CYPHER | RUSTAGE ft. Shao Dow, Shwabadi &amp; More [ONE PIECE]",
-  //     duration: 366,
-  //     uploader: "RUSTAGE",
-  //     thumbnail: "https://i.ytimg.com/vi/U7bp_aiVBGU/hqdefault.jpg",
-  //     webpage_url: "https://www.youtube.com/watch?v=U7bp_aiVBGU",
-  //     upload_date: "2023-10-14T16:00:18Z",
-  //     largest_thumbnail: "https://i.ytimg.com/vi/U7bp_aiVBGU/hqdefault.jpg",
-  //     smallest_thumbnail: "https://i.ytimg.com/vi/U7bp_aiVBGU/default.jpg",
-  //   },
-  //   {
-  //     title: "RUSTAGE - LEADERS OF HISTORY RAP CYPHER | Reaction!",
-  //     duration: 1398,
-  //     uploader: "WHAT IT DO DAVE",
-  //     thumbnail: "https://i.ytimg.com/vi/ctU9akacRCw/hqdefault.jpg",
-  //     webpage_url: "https://www.youtube.com/watch?v=ctU9akacRCw",
-  //     upload_date: "2025-06-22T15:00:58Z",
-  //     largest_thumbnail: "https://i.ytimg.com/vi/ctU9akacRCw/hqdefault.jpg",
-  //     smallest_thumbnail: "https://i.ytimg.com/vi/ctU9akacRCw/default.jpg",
-  //   },
-  // ];
-
-  const [openMenuIndex, setOpenMenuIndex] = useState(null);
 
   const openItem = (term) => {
     submitSearch(term);
-    setOpenMenuIndex(null);
   };
 
   const copyItem = (term) => {
@@ -169,7 +62,6 @@ export const HomeSideBar = () => {
     } catch (e) {
       console.warn("Clipboard error", e);
     }
-    setOpenMenuIndex(null);
   };
 
   const deleteItem = (index) => {
@@ -177,7 +69,6 @@ export const HomeSideBar = () => {
     if (confirmed) {
       removeSearchAt(index);
     }
-    setOpenMenuIndex(null);
   };
 
   const playDownload = (download) => {
@@ -191,7 +82,6 @@ export const HomeSideBar = () => {
     // idx = (page - 1) * pageSize + idx;
     setQueueFromSearchResults([download?.song], /*startIndex=*/ 0); // sets queue and currentIndex to idx
     showMiniForIndex(0, true); // opens the mini player but doesn't auto-play
-    setOpenMenuIndex(null);
   };
 
   // const onPlay = (idx) => {};
@@ -201,7 +91,6 @@ export const HomeSideBar = () => {
     if (confirmed) {
       removeDownload(download?.id || download?.song?.id || download);
     }
-    setOpenMenuIndex(null);
   };
 
   return (
@@ -330,6 +219,27 @@ export const HomeSideBar = () => {
                 ]}
               >
                 <View style={[styles.imageBox]}>
+                  {getDownloadStatus(download?.id) === "pending" && (
+                    <View
+                      // source={{ uri: download?.song?.largest_thumbnail }}
+                      style={{
+                        zIndex: 250,
+                        position: "absolute",
+                        inset: 0,
+                        width: "100%",
+                        height: "100%",
+                        backgroundColor: HEXA(theme.background, 0.6),
+                        display: "grid",
+                        placeContent: "center",
+                      }}
+                    >
+                      <ActivityIndicator
+                        size={25}
+                        color={theme.text}
+                        // color={accentColors[accentKey].dark}
+                      />
+                    </View>
+                  )}{" "}
                   <Image
                     style={[styles.image]}
                     source={{ uri: download?.song?.largest_thumbnail }}
